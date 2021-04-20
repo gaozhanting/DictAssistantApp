ea7036f6e8f752fbb7a943eedc28156eae74e795
colorgmi <colorgmi@gmail.com>
2021-04-20 17:55:49 +0800

    Initial Commit

---
+++ DictAssistantApp/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  DictAssistantApp
+//
+//  Created by Gao Cong on 2021/4/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
