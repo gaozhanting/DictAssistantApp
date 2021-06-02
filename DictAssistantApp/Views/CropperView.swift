d195fb30900b64f7cf6fc950f69066bd1e34b7ff
GaoZhanting <colorgmi@icloud.com>
2021-06-02 15:37:14 +0800

    feature: 1. one overlay for move & one overlay for scale 2. realTranslationWidth & realTranslationHeight must comply minWidth & minHeight

--- DictAssistantApp/Views/CropperView.swift
+++ DictAssistantApp/Views/CropperView.swift
@@ -14,16 +14,6 @@ struct CropperView: View {
     @GestureState private var startWidth: CGFloat? = nil
     @GestureState private var startHeight: CGFloat? = nil
 
-    
-    var info: some View {
-        VStack {
-            Text("Location: \(Int(cropData.x)), \(Int(cropData.y))")
-            Text("WH: \(Int(cropData.width)), \(Int(cropData.height))")
-        }
-        .background(Color.yellow)
-        .frame(width: 300, height: 120)
-    }
-    
     var body: some View {
         ZStack(alignment: Alignment(horizontal: .center, vertical: .center)) {
             curtain
@@ -36,66 +26,87 @@ struct CropperView: View {
     
     var curtain: some View {
         Rectangle()
-            .fill(Color.gray)
-            .opacity(0)
+//            .fill(Color.gray)
+            .opacity(0) // this will make curtain not response to the cursor scroll event
     }
     
     // Runtime Warning: Invalid frame dimension (negative or non-finite).
     var cropper: some View {
         Rectangle()
-            .fill(Color.secondary)
-            .opacity(0.01)
-//            .border(Color.secondary, width: 0.3)
+//            .fill(Color.secondary)
+            .opacity(0) // this will make cropper not response to the cursor scroll event
             .frame(width: cropData.width, height: cropData.height)
-            .overlay(
+            .overlay( // this will add a stoke border of cropper
                 Rectangle()
-                    .strokeBorder(style: StrokeStyle(lineWidth: 2, dash: [5]))
-                    .foregroundColor(.primary)
+                    .strokeBorder(style: StrokeStyle(lineWidth: 1, dash: [2]))
             )
-            .onHover { hovered in
-                if hovered {
-                    NSCursor.openHand.set()
-                } else {
-                    NSCursor.arrow.set()
-                }
-            }
+//            .onHover { hovered in // not works
+//                if hovered {
+//                    NSCursor.openHand.set()
+//                } else {
+//                    NSCursor.arrow.set()
+//                }
+//            }
 //            .overlay(info, alignment: .center) // for test ?!
             .overlay(
-                Circle()
-                    .opacity(0.01)
-                    .frame(width: 30, height: 30)
-//                    .foregroundColor(.blue)
-//                    .border(Color.green)
-                    .offset(x: 5, y: 5)
-                    .gesture(resizeDragGesture),
+                Rectangle()
+//                    .opacity(0.01)
+                    .frame(width: 25, height: 25)
+                    .foregroundColor(.green)
+                    .border(Color.red)
+                    .gesture(move),
+                alignment: .bottomTrailing)
+            .overlay(
+                Rectangle()
+//                    .opacity(0.01)
+                    .frame(width: 15, height: 15)
+                    .foregroundColor(.blue)
+                    .border(Color.red)
+                    .offset(x: 8, y: 8)
+                    .gesture(scale),
                 alignment: .bottomTrailing)
             .position(CGPoint(x: cropData.x, y: cropData.y))
-            .gesture(drag)
-
     }
     
-//    func cursorNearbyRightEdgeOfRectangle() -> Bool {
-//        return true
-//    }
+    var info: some View {
+        VStack {
+            Text("Location: \(Int(cropData.x)), \(Int(cropData.y))")
+            Text("WH: \(Int(cropData.width)), \(Int(cropData.height))")
+        }
+        .background(Color.yellow)
+        .frame(width: 300, height: 120)
+    }
     
-    var resizeDragGesture: some Gesture {
+    private let minWidth: CGFloat = 100.0
+    private let minHeight: CGFloat = 50.0
+    
+    var scale: some Gesture {
         DragGesture(coordinateSpace: .named("stack"))
             .onChanged { value in
+                var newWidth = startWidth ?? cropData.width
+                var realTranslationWidth = value.translation.width
+                if (newWidth + realTranslationWidth) < minWidth {
+                    realTranslationWidth = minWidth - newWidth
+                }
+                newWidth += realTranslationWidth
+
+                var newHeight = startHeight ?? cropData.height
+                var realTranslationHeight = value.translation.height
+                if (newHeight + realTranslationHeight) < minHeight {
+                    realTranslationHeight = minHeight - newHeight
+                }
+                newHeight += realTranslationHeight
+
                 var newPosition = startLocation ?? CGPoint(x: cropData.x, y: cropData.y)
-                newPosition.x += 0.5 * value.translation.width
-                newPosition.y += 0.5 * value.translation.height
+                newPosition.x += 0.5 * realTranslationWidth
+                newPosition.y += 0.5 * realTranslationHeight
+
+                cropData.width = newWidth
+                cropData.height = newHeight
                 cropData.x = newPosition.x
                 cropData.y = newPosition.y
-            }
-            .onChanged { value in
-                var newWidth = startWidth ?? cropData.width
-                newWidth += value.translation.width
-                cropData.width = newWidth
-            }
-            .onChanged { value in
-                var newHeight = startHeight ?? cropData.height
-                newHeight += value.translation.height
-                cropData.height = newHeight
+                
+//                print("scale> x:\(cropData.x), y:\(cropData.y), width:\(cropData.width), height:\(cropData.height)")
             }
             .updating($startLocation) { (value, startLocation, transaction) in
                 startLocation = startLocation ?? CGPoint(x: cropData.x, y: cropData.y)
@@ -108,7 +119,7 @@ struct CropperView: View {
             }
     }
     
-    var drag: some Gesture {
+    var move: some Gesture {
         DragGesture(coordinateSpace: .named("stack"))
             .onChanged { value in
                 var newPosition = startLocation ?? CGPoint(x: cropData.x, y: cropData.y)
@@ -116,6 +127,8 @@ struct CropperView: View {
                 newPosition.y += value.translation.height
                 cropData.x = newPosition.x
                 cropData.y = newPosition.y
+                
+//                print("move> x:\(cropData.x), y:\(cropData.y), width:\(cropData.width), height:\(cropData.height)")
             }
             .updating($startLocation) { (value, startLocation, transaction) in
                 startLocation = startLocation ?? CGPoint(x: cropData.x, y: cropData.y)
