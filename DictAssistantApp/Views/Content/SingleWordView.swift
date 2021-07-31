a41fddfb7da00bd88ba4586c6e03b995109d1bdf
GaoZhanting <colorgmi@icloud.com>
2021-07-31 18:25:59 +0800

    refactor: break the single word view into smaller view

--- DictAssistantApp/Views/Content/SingleWordView.swift
+++ DictAssistantApp/Views/Content/SingleWordView.swift
@@ -8,27 +8,26 @@
 import SwiftUI
 
 struct SingleWordView: View {
-    @Environment(\.openURL) var openURL
-    @Environment(\.addToKnownWords) var addToKnownWords
-    @Environment(\.removeFromKnownWords) var removeFromKnownWords
-    @AppStorage(IsAddLineBreakKey) private var isAddLineBreak: Bool = true
-    @AppStorage(FontRateKey) private var fontRate: Double = 0.6
     @AppStorage(ContentStyleKey) private var contentStyle: ContentStyle = .portrait
-    @Environment(\.colorScheme) var colorScheme
 
     let wordCell: WordCell
 
-    var isKnown: IsKnown {
-        wordCell.isKnown
+    var body: some View {
+        switch contentStyle {
+        case .portrait:
+            TextBody(wordCell: wordCell)
+
+        case .landscape:
+            VStack {
+                TextBody(wordCell: wordCell)
+                Spacer()
+            }
+        }
     }
-    
-    var unKnown: Bool {
-        isKnown == .unKnown
-    }
-    
-    var known: Bool {
-        isKnown == .known
-    }
+}
+
+fileprivate struct TextBody: View {
+    let wordCell: WordCell
     
     var word: String {
         wordCell.word
@@ -38,14 +37,24 @@ struct SingleWordView: View {
         word.contains(" ")
     }
     
-    var trans: String {
-        wordCell.trans
+    var known: Bool {
+        wordCell.isKnown == .known
     }
     
-    var transText: String {
-        isAddLineBreak ? "\n" + trans : trans
+    var unKnown: Bool {
+        wordCell.isKnown == .unKnown
     }
+    
+    @AppStorage(ContentBackgroundDisplayKey) private var contentBackgroundDisplay: Bool = false
+    @AppStorage(BackgroundColorKey) private var backgroundColor: Data = colorToData(NSColor.clear)!
+    var theBackgroundColor: Color {
+        Color(dataToColor(backgroundColor)!)
+    }
+    
+    @Environment(\.addToKnownWords) var addToKnownWords
+    @Environment(\.removeFromKnownWords) var removeFromKnownWords
 
+    @Environment(\.openURL) var openURL
     func openExternalDict(_ word: String) {
         let replaceSpaced = word.replacingOccurrences(of: " ", with: "-")
         guard let url = URL(string: "https://www.collinsdictionary.com/dictionary/english/\(replaceSpaced)") else {
@@ -54,7 +63,56 @@ struct SingleWordView: View {
         }
         openURL(url)
     }
+
+    var body: some View {
+        TextWithShadow(wordCell: wordCell)
+            .opacity( (known && isPhrase) ? 0.5 : 1)
+            .padding(.vertical, 4)
+            .padding(.horizontal, 6)
+            .contextMenu {
+                Button(unKnown ? "Add to Known" : "Remove from known", action: {
+                    unKnown ? addToKnownWords(word) : removeFromKnownWords(word)
+                })
+                Menu("Online Dict Link") {
+                    Button("Collins", action: { openExternalDict(word) })
+                }
+            }
+            .onTapGesture(count: 2) {
+                openDict(word)
+            }
+            .background(contentBackgroundDisplay ? nil : theBackgroundColor)
+    }
+}
+
+fileprivate struct TextWithShadow: View {
+    @AppStorage(ShadowColorKey) private var shadowColor: Data = colorToData(NSColor.labelColor)!
+    @AppStorage(ShadowRadiusKey) private var shadowRadius: Double = 3
+    @AppStorage(ShadowXOffSetKey) private var shadowXOffset: Double = 0
+    @AppStorage(ShadowYOffSetKey) private var shadowYOffset: Double = 2
+
+    @AppStorage(TextShadowToggleKey) private var textShadowToggle: Bool = false
     
+    let wordCell: WordCell
+    
+    var body: some View {
+        if textShadowToggle {
+            TheText(wordCell: wordCell)
+                .shadow(
+                    color: Color(dataToColor(shadowColor)!), // Color(NSColor.labelColor.withAlphaComponent(0.3)), /// shadow color
+                    radius: CGFloat(shadowRadius), /// shadow radius
+                    x: CGFloat(shadowXOffset), //0, /// x offset
+                    y: CGFloat(shadowYOffset) //2 /// y offset
+                )
+        } else {
+            TheText(wordCell: wordCell)
+        }
+    }
+}
+
+// refer:
+// title title2 : landscape
+// headline callout : portrait
+fileprivate struct TheText: View {
     @AppStorage(WordColorKey) private var wordColor: Data = colorToData(NSColor.labelColor.withAlphaComponent(0.3))!
     @AppStorage(TransColorKey) private var transColor: Data = colorToData(NSColor.highlightColor)!
     var theWordColor: Color {
@@ -72,16 +130,33 @@ struct SingleWordView: View {
         Font(dataToFont(fontData)!)
     }
     
+    @AppStorage(FontRateKey) private var fontRate: Double = 0.6
     var transFont: Font {
         let font = dataToFont(fontData)!
         let result = NSFont.init(name: font.fontName, size: font.pointSize * CGFloat(fontRate))!
         return Font(result)
     }
-
-    // title title2 : landscape
-    // headline callout : portrait
     
-    var textView: Text {
+    let wordCell: WordCell
+
+    var unKnown: Bool {
+        wordCell.isKnown == .unKnown
+    }
+    
+    var word: String {
+        wordCell.word
+    }
+    
+    var trans: String {
+        wordCell.trans
+    }
+    
+    @AppStorage(IsAddLineBreakKey) private var isAddLineBreak: Bool = true
+    var transText: String {
+        isAddLineBreak ? "\n" + trans : trans
+    }
+
+    var body: Text {
         unKnown ?
             Text(word)
             .foregroundColor(theWordColor)
@@ -93,70 +168,6 @@ struct SingleWordView: View {
             Text(word)
             .foregroundColor(theKnownWordColor)
             .font(font)
-    }
-    
-    @AppStorage(BackgroundColorKey) private var backgroundColor: Data = colorToData(NSColor.clear)!
-    var theBackgroundColor: Color {
-        Color(dataToColor(backgroundColor)!)
-    }
-    
-    @AppStorage(ContentBackgroundDisplayKey) private var contentBackgroundDisplay: Bool = false
-    
-    var TextBody: some View {
-        WithShadowText(textView: textView)
-            .opacity( (known && isPhrase) ? 0.5 : 1)
-            .padding(.vertical, 4)
-            .padding(.horizontal, 6)
-            .contextMenu {
-                Button(unKnown ? "Add to Known" : "Remove from known", action: {
-                    unKnown ? addToKnownWords(word) : removeFromKnownWords(word)
-                })
-                Menu("Online Dict Link") {
-                    Button("Collins", action: { openExternalDict(word) })
-                }
-            }
-            .onTapGesture(count: 2) {
-                openDict(word)
-            }
-            .background(contentBackgroundDisplay ? nil : theBackgroundColor)
-    }
-
-    var body: some View {
-        switch contentStyle {
-        case .portrait:
-            TextBody
-
-        case .landscape:
-            VStack {
-                TextBody
-                Spacer()
-            }
-        }
-    }
-}
-
-fileprivate struct WithShadowText: View {
-    @AppStorage(ShadowColorKey) private var shadowColor: Data = colorToData(NSColor.labelColor)!
-    @AppStorage(ShadowRadiusKey) private var shadowRadius: Double = 3
-    @AppStorage(ShadowXOffSetKey) private var shadowXOffset: Double = 0
-    @AppStorage(ShadowYOffSetKey) private var shadowYOffset: Double = 2
-
-    @AppStorage(TextShadowToggleKey) private var textShadowToggle: Bool = false
-    
-    let textView: Text
-    
-    var body: some View {
-        if textShadowToggle {
-            textView
-                .shadow(
-                    color: Color(dataToColor(shadowColor)!), // Color(NSColor.labelColor.withAlphaComponent(0.3)), /// shadow color
-                    radius: CGFloat(shadowRadius), /// shadow radius
-                    x: CGFloat(shadowXOffset), //0, /// x offset
-                    y: CGFloat(shadowYOffset) //2 /// y offset
-                )
-        } else {
-            textView
-        }
     }
 }
 
