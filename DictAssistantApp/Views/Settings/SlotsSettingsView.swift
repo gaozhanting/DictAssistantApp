31176fa81d9f322dba857b0f5d443532cdedcb2d
GaoZhanting <colorgmi@icloud.com>
2021-08-08 18:20:10 +0800

    refactor

--- DictAssistantApp/Views/Settings/SlotsSettingsView.swift
+++ DictAssistantApp/Views/Settings/SlotsSettingsView.swift
@@ -142,12 +142,88 @@ fileprivate let defaultSettings = Settings(
     contentFrame: NSRect(x: 300, y: 300, width: 600, height: 200)
 )
 
+fileprivate func getCurrentSettings() -> Settings {
+    return Settings(
+        tRTextRecognitionLevel: UserDefaults.standard.integer(forKey: TRTextRecognitionLevelKey),
+        tRMinimumTextHeight: UserDefaults.standard.double(forKey: TRMinimumTextHeightKey),
+        isWithAnimation: UserDefaults.standard.bool(forKey: IsWithAnimationKey),
+        isShowPhrases: UserDefaults.standard.bool(forKey: IsShowPhrasesKey),
+        isShowCurrentKnown: UserDefaults.standard.bool(forKey: IsShowCurrentKnownKey),
+        cropperStyle: CropperStyle(rawValue: UserDefaults.standard.integer(forKey: CropperStyleKey))!,
+        contentStyle: ContentStyle(rawValue: UserDefaults.standard.integer(forKey: ContentStyleKey))!,
+        isShowWindowShadow: UserDefaults.standard.bool(forKey: IsShowWindowShadowKey),
+        contentBackgroundVisualEffect: UserDefaults.standard.bool(forKey: ContentBackgroundVisualEffectKey),
+        contentBackGroundVisualEffectMaterial: UserDefaults.standard.integer(forKey: ContentBackGroundVisualEffectMaterialKey),
+        wordColor: UserDefaults.standard.data(forKey: WordColorKey)!,
+        transColor: UserDefaults.standard.data(forKey: TransColorKey)!,
+        backgroundColor: UserDefaults.standard.data(forKey: BackgroundColorKey)!,
+        portraitCorner: PortraitCorner(rawValue: UserDefaults.standard.integer(forKey: PortraitCornerKey))!,
+        showToastToggle: UserDefaults.standard.bool(forKey: ShowToastToggleKey),
+        font: UserDefaults.standard.data(forKey: FontKey)!,
+        fontRate: UserDefaults.standard.double(forKey: FontRateKey),
+        shadowColor: UserDefaults.standard.data(forKey: ShadowColorKey)!,
+        shadowRadius: UserDefaults.standard.double(forKey: ShadowRadiusKey),
+        shadowXOffSet: UserDefaults.standard.double(forKey: ShadowXOffSetKey),
+        shadowYOffSet: UserDefaults.standard.double(forKey: ShadowYOffSetKey),
+        textShadowToggle: UserDefaults.standard.bool(forKey: TextShadowToggleKey),
+        portraitMaxHeight: UserDefaults.standard.double(forKey: PortraitMaxHeightKey),
+        landscapeMaxWidth: UserDefaults.standard.double(forKey: LandscapeMaxWidthKey),
+        speakWordToggle: UserDefaults.standard.bool(forKey: SpeakWordToggleKey),
+        theColorScheme: TheColorScheme(rawValue: UserDefaults.standard.string(forKey: TheColorSchemeKey)!)!,
+        cropperFrame: cropperWindow.frame,
+        contentFrame: contentWindow.frame
+    )
+}
+
 fileprivate enum Slot: String, CaseIterable, Identifiable {
     case blue
     case green
     case red
 
     var id: String { self.rawValue }
+}
+
+fileprivate func theColor(from slot: Slot) -> Color {
+    switch slot {
+    case .blue:
+        return Color.blue
+    case .green:
+        return Color.green
+    case .red:
+        return Color.red
+    }
+}
+
+// write settings, read slot
+fileprivate func dumpSettings(from s: Settings) {
+    UserDefaults.standard.set(s.tRTextRecognitionLevel, forKey: TRTextRecognitionLevelKey)
+    UserDefaults.standard.set(s.tRMinimumTextHeight, forKey: TRMinimumTextHeightKey)
+    UserDefaults.standard.set(s.isWithAnimation, forKey: IsWithAnimationKey)
+    UserDefaults.standard.set(s.isShowPhrases, forKey: IsShowPhrasesKey)
+    UserDefaults.standard.set(s.isShowCurrentKnown, forKey: IsShowCurrentKnownKey)
+    UserDefaults.standard.set(s.cropperStyle.rawValue, forKey: CropperStyleKey)
+    UserDefaults.standard.set(s.contentStyle.rawValue, forKey: ContentStyleKey)
+    UserDefaults.standard.set(s.isShowWindowShadow, forKey: IsShowWindowShadowKey)
+    UserDefaults.standard.set(s.contentBackgroundVisualEffect, forKey: ContentBackgroundVisualEffectKey)
+    UserDefaults.standard.set(s.contentBackGroundVisualEffectMaterial, forKey: ContentBackgroundVisualEffectKey)
+    UserDefaults.standard.set(s.wordColor, forKey: WordColorKey)
+    UserDefaults.standard.set(s.transColor, forKey: TransColorKey)
+    UserDefaults.standard.set(s.backgroundColor, forKey: BackgroundColorKey)
+    UserDefaults.standard.set(s.portraitCorner.rawValue, forKey: PortraitCornerKey)
+    UserDefaults.standard.set(s.showToastToggle, forKey: ShowToastToggleKey)
+    UserDefaults.standard.set(s.font, forKey: FontKey)
+    UserDefaults.standard.set(s.fontRate, forKey: FontRateKey)
+    UserDefaults.standard.set(s.shadowColor, forKey: ShadowColorKey)
+    UserDefaults.standard.set(s.shadowRadius, forKey: ShadowRadiusKey)
+    UserDefaults.standard.set(s.shadowXOffSet, forKey: ShadowXOffSetKey)
+    UserDefaults.standard.set(s.shadowYOffSet, forKey: ShadowYOffSetKey)
+    UserDefaults.standard.set(s.textShadowToggle, forKey: TextShadowToggleKey)
+    UserDefaults.standard.set(s.portraitMaxHeight, forKey: PortraitMaxHeightKey)
+    UserDefaults.standard.set(s.landscapeMaxWidth, forKey: LandscapeMaxWidthKey)
+    UserDefaults.standard.set(s.speakWordToggle, forKey: SpeakWordToggleKey)
+    UserDefaults.standard.set(s.theColorScheme.rawValue, forKey: TheColorSchemeKey)
+    cropperWindow.setFrame(s.cropperFrame, display: true)
+    contentWindow.setFrame(s.contentFrame, display: true)
 }
 
 struct SlotsSettingsView: View {
@@ -162,188 +238,30 @@ struct SlotsSettingsView: View {
 
 fileprivate struct SlotsSettings: View {
     @AppStorage(SelectedSlotKey) private var selectedSlot = Slot.blue
-    @AppStorage(BlueSettingsKey) private var blueSettingsData: Data = settingsToData(defaultSettings)!
-    @AppStorage(GreenSettingsKey) private var greenSettingsData: Data = settingsToData(defaultSettings)!
-    @AppStorage(RedSettingsKey) private var redSettingsData: Data = settingsToData(defaultSettings)!
-    @EnvironmentObject var statusData: StatusData
     
-    var isPlaying: Bool {
-        statusData.isPlaying
-    }
-    
-    var blueSettings: Settings {
-        dataToSettings(blueSettingsData)!
-    }
-    
-    var greenSettings: Settings {
-        dataToSettings(greenSettingsData)!
-    }
-    
-    var redSettings: Settings {
-        dataToSettings(redSettingsData)!
-    }
-
-    func whichSetting(_ slot: Slot) -> Settings {
-        switch slot {
-        case .blue:
-            return blueSettings
-        case .green:
-            return greenSettings
-        case .red:
-            return redSettings
-        }
-    }
-
-    // write settings, read slot
-    func selectSlot(_ slot: Slot) {
-        let s = whichSetting(slot)
-        UserDefaults.standard.set(s.tRTextRecognitionLevel, forKey: TRTextRecognitionLevelKey)
-        UserDefaults.standard.set(s.tRMinimumTextHeight, forKey: TRMinimumTextHeightKey)
-        UserDefaults.standard.set(s.isWithAnimation, forKey: IsWithAnimationKey)
-        UserDefaults.standard.set(s.isShowPhrases, forKey: IsShowPhrasesKey)
-        UserDefaults.standard.set(s.isShowCurrentKnown, forKey: IsShowCurrentKnownKey)
-        UserDefaults.standard.set(s.cropperStyle.rawValue, forKey: CropperStyleKey)
-        UserDefaults.standard.set(s.contentStyle.rawValue, forKey: ContentStyleKey)
-        UserDefaults.standard.set(s.isShowWindowShadow, forKey: IsShowWindowShadowKey)
-        UserDefaults.standard.set(s.contentBackgroundVisualEffect, forKey: ContentBackgroundVisualEffectKey)
-        UserDefaults.standard.set(s.contentBackGroundVisualEffectMaterial, forKey: ContentBackgroundVisualEffectKey)
-        UserDefaults.standard.set(s.wordColor, forKey: WordColorKey)
-        UserDefaults.standard.set(s.transColor, forKey: TransColorKey)
-        UserDefaults.standard.set(s.backgroundColor, forKey: BackgroundColorKey)
-        UserDefaults.standard.set(s.portraitCorner.rawValue, forKey: PortraitCornerKey)
-        UserDefaults.standard.set(s.showToastToggle, forKey: ShowToastToggleKey)
-        UserDefaults.standard.set(s.font, forKey: FontKey)
-        UserDefaults.standard.set(s.fontRate, forKey: FontRateKey)
-        UserDefaults.standard.set(s.shadowColor, forKey: ShadowColorKey)
-        UserDefaults.standard.set(s.shadowRadius, forKey: ShadowRadiusKey)
-        UserDefaults.standard.set(s.shadowXOffSet, forKey: ShadowXOffSetKey)
-        UserDefaults.standard.set(s.shadowYOffSet, forKey: ShadowYOffSetKey)
-        UserDefaults.standard.set(s.textShadowToggle, forKey: TextShadowToggleKey)
-        UserDefaults.standard.set(s.portraitMaxHeight, forKey: PortraitMaxHeightKey)
-        UserDefaults.standard.set(s.landscapeMaxWidth, forKey: LandscapeMaxWidthKey)
-        UserDefaults.standard.set(s.speakWordToggle, forKey: SpeakWordToggleKey)
-        UserDefaults.standard.set(s.theColorScheme.rawValue, forKey: TheColorSchemeKey)
-        cropperWindow.setFrame(s.cropperFrame, display: true)
-        contentWindow.setFrame(s.contentFrame, display: true)
-    }
-    
-    var binding: Binding<Slot> {
-        Binding.init(
-            get: { selectedSlot },
-            set: { newValue in
-                selectedSlot = newValue
-                selectSlot(selectedSlot)
-            }
-        )
-    }
-
-    var body: some View {
-        Picker("", selection: binding) {
-            SlotView(color: .blue, selectedSlot: $selectedSlot)
-                .tag(Slot.blue)
-            
-            SlotView(color: .green, selectedSlot: $selectedSlot)
-                .tag(Slot.green)
-            
-            SlotView(color: .red, selectedSlot: $selectedSlot)
-                .tag(Slot.red)
-        }
-        .labelsHidden()
-        .pickerStyle(RadioGroupPickerStyle())
-        .disabled(isPlaying)
-    }
-}
-
-fileprivate struct SlotView: View {
     @AppStorage(BlueLabelKey) private var blueLabel: String = ""
     @AppStorage(GreenLabelKey) private var greenLabel: String = ""
     @AppStorage(RedLabelKey) private var redLabel: String = ""
     @AppStorage(BlueSettingsKey) private var blueSettingsData: Data = settingsToData(defaultSettings)!
     @AppStorage(GreenSettingsKey) private var greenSettingsData: Data = settingsToData(defaultSettings)!
     @AppStorage(RedSettingsKey) private var redSettingsData: Data = settingsToData(defaultSettings)!
-    
-    var blueSettings: Settings {
-        dataToSettings(blueSettingsData)!
-    }
-    
-    var greenSettings: Settings {
-        dataToSettings(greenSettingsData)!
-    }
-    
-    var redSettings: Settings {
-        dataToSettings(redSettingsData)!
-    }
-    
-    func whichSetting(_ slot: Slot) -> Settings {
-        switch slot {
-        case .blue:
-            return blueSettings
-        case .green:
-            return greenSettings
-        case .red:
-            return redSettings
-        }
-    }
-    
-    let color: Color
-    @Binding var selectedSlot: Slot
-    
-    // write slot, read settings
-    func storeSlot(_ slot: Slot) {
-        let currentSettings = Settings(
-            tRTextRecognitionLevel: UserDefaults.standard.integer(forKey: TRTextRecognitionLevelKey),
-            tRMinimumTextHeight: UserDefaults.standard.double(forKey: TRMinimumTextHeightKey),
-            isWithAnimation: UserDefaults.standard.bool(forKey: IsWithAnimationKey),
-            isShowPhrases: UserDefaults.standard.bool(forKey: IsShowPhrasesKey),
-            isShowCurrentKnown: UserDefaults.standard.bool(forKey: IsShowCurrentKnownKey),
-            cropperStyle: CropperStyle(rawValue: UserDefaults.standard.integer(forKey: CropperStyleKey))!,
-            contentStyle: ContentStyle(rawValue: UserDefaults.standard.integer(forKey: ContentStyleKey))!,
-            isShowWindowShadow: UserDefaults.standard.bool(forKey: IsShowWindowShadowKey),
-            contentBackgroundVisualEffect: UserDefaults.standard.bool(forKey: ContentBackgroundVisualEffectKey),
-            contentBackGroundVisualEffectMaterial: UserDefaults.standard.integer(forKey: ContentBackGroundVisualEffectMaterialKey),
-            wordColor: UserDefaults.standard.data(forKey: WordColorKey)!,
-            transColor: UserDefaults.standard.data(forKey: TransColorKey)!,
-            backgroundColor: UserDefaults.standard.data(forKey: BackgroundColorKey)!,
-            portraitCorner: PortraitCorner(rawValue: UserDefaults.standard.integer(forKey: PortraitCornerKey))!,
-            showToastToggle: UserDefaults.standard.bool(forKey: ShowToastToggleKey),
-            font: UserDefaults.standard.data(forKey: FontKey)!,
-            fontRate: UserDefaults.standard.double(forKey: FontRateKey),
-            shadowColor: UserDefaults.standard.data(forKey: ShadowColorKey)!,
-            shadowRadius: UserDefaults.standard.double(forKey: ShadowRadiusKey),
-            shadowXOffSet: UserDefaults.standard.double(forKey: ShadowXOffSetKey),
-            shadowYOffSet: UserDefaults.standard.double(forKey: ShadowYOffSetKey),
-            textShadowToggle: UserDefaults.standard.bool(forKey: TextShadowToggleKey),
-            portraitMaxHeight: UserDefaults.standard.double(forKey: PortraitMaxHeightKey),
-            landscapeMaxWidth: UserDefaults.standard.double(forKey: LandscapeMaxWidthKey),
-            speakWordToggle: UserDefaults.standard.bool(forKey: SpeakWordToggleKey),
-            theColorScheme: TheColorScheme(rawValue: UserDefaults.standard.string(forKey: TheColorSchemeKey)!)!,
-            cropperFrame: cropperWindow.frame,
-            contentFrame: contentWindow.frame
-        )
-        
+    var selectedSettings: Settings {
         switch selectedSlot {
         case .blue:
-            blueSettingsData = settingsToData(currentSettings)!
+            return dataToSettings(blueSettingsData)!
         case .green:
-            greenSettingsData = settingsToData(currentSettings)!
+            return dataToSettings(greenSettingsData)!
         case .red:
-            redSettingsData = settingsToData(currentSettings)!
+            return dataToSettings(redSettingsData)!
         }
     }
     
-    var isColorSelected: Bool {
-        if color == .blue {
-            return selectedSlot.rawValue == "blue"
-        }
-        if color == .green {
-            return selectedSlot.rawValue == "green"
-        }
-        if color == .red {
-            return selectedSlot.rawValue == "red"
-        }
-        return false // not this case
+    @EnvironmentObject var statusData: StatusData
+    var isPlaying: Bool {
+        statusData.isPlaying
     }
     
+    // isShowStoreButton need these almost all @AppStorage data
     @AppStorage(TRTextRecognitionLevelKey) private var tRTextRecognitionLevel: VNRequestTextRecognitionLevel = .fast // fast 1, accurate 0
     @AppStorage(TRMinimumTextHeightKey) private var tRMinimumTextHeight: Double = systemDefaultMinimumTextHeight // 0.0315
     @AppStorage(IsWithAnimationKey) private var isWithAnimation: Bool = true
@@ -371,8 +289,23 @@ fileprivate struct SlotView: View {
     @AppStorage(SpeakWordToggleKey) private var speakWordToggle: Bool = false
     @AppStorage(TheColorSchemeKey) private var theColorScheme: TheColorScheme = .system
 
-    var isTheSame: Bool {
-        let s = whichSetting(selectedSlot)
+    // write slot, read settings
+    func storeSlot(_ slot: Slot) -> () -> Void {
+        return {
+            let currentSettings = getCurrentSettings()
+            switch slot {
+            case .blue:
+                blueSettingsData = settingsToData(currentSettings)!
+            case .green:
+                greenSettingsData = settingsToData(currentSettings)!
+            case .red:
+                redSettingsData = settingsToData(currentSettings)!
+            }
+        }
+    }
+
+    func isSelectedSlotEqualWithCurrentSettings() -> Bool {
+        let s = selectedSettings
         return s.tRTextRecognitionLevel == tRTextRecognitionLevel.rawValue &&
             s.tRMinimumTextHeight == tRMinimumTextHeight &&
             s.isWithAnimation == isWithAnimation &&
@@ -402,19 +335,58 @@ fileprivate struct SlotView: View {
 //            s.cropperFrame == cropperWindow.frame && // crash for SwiftUI Preview, cause there is no cropperWindow
 //            s.contentFrame == contentWindow.frame
     }
-   
-    func whichLabel(_ color: Color) -> Binding<String> {
-        switch color {
-        case .blue:
-            return $blueLabel
-        case .green:
-            return $greenLabel
-        case .red:
-            return $redLabel
-        default: // not this case
-            return $blueLabel
-        }
+    
+    func isShowStoreButton(_ slot: Slot) -> Bool {
+        selectedSlot == slot && !isSelectedSlotEqualWithCurrentSettings()
     }
+   
+    var binding: Binding<Slot> {
+        Binding.init(
+            get: { selectedSlot },
+            set: { newValue in
+                selectedSlot = newValue
+                dumpSettings(from: selectedSettings)
+            }
+        )
+    }
+    
+    var body: some View {
+        Picker("", selection: binding) {
+            SlotItem(
+                color: theColor(from: .blue),
+                label: $blueLabel,
+                isShowStoreButton: isShowStoreButton(.blue),
+                storeAction: storeSlot(.blue)
+            )
+            .tag(Slot.blue)
+            
+            SlotItem(
+                color: theColor(from: .green),
+                label: $greenLabel,
+                isShowStoreButton: isShowStoreButton(.green),
+                storeAction: storeSlot(.green)
+            )
+            .tag(Slot.green)
+            
+            SlotItem(
+                color: theColor(from: .red),
+                label: $redLabel,
+                isShowStoreButton: isShowStoreButton(.red),
+                storeAction: storeSlot(.red)
+            )
+            .tag(Slot.red)
+        }
+        .labelsHidden()
+        .pickerStyle(RadioGroupPickerStyle())
+        .disabled(isPlaying)
+    }
+}
+
+struct SlotItem: View {
+    let color: Color
+    @Binding var label: String
+    let isShowStoreButton: Bool
+    let storeAction: () -> Void
     
     var body: some View {
         HStack {
@@ -423,13 +395,13 @@ fileprivate struct SlotView: View {
                     .font(.title)
                     .foregroundColor(color)
                 
-                TextField("", text: whichLabel(color))
-                    .frame(width: 70)
+                TextField("", text: $label)
+                    .frame(width: 110)
             }
             
-            if isColorSelected && !isTheSame {
+            if isShowStoreButton {
                 Button("store", action: {
-                    storeSlot(selectedSlot)
+                    storeAction()
                 })
             }
         }
