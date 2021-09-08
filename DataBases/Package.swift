1512e1e776150c123f5d405ed0555a2f56d6fdc2
GaoZhanting <colorgmi@icloud.com>
2021-09-08 11:06:49 +0800

    no more need this

--- DataBases/Package.swift
+++ DataBases/Package.swift
@@ -25,7 +25,6 @@ let package = Package(
                 .process("Resources/oxford_3000.txt"),
                 .process("Resources/first_100_000_of_enwiki-20190320-words-frequency.txt"),
                 .process("Resources/lemma.en.txt"),
-                .process("Resources/words_should_have_lemma.txt"),
                 .process("Resources/phrases_and_idioms_extracted_from_brief_oxford_dict.txt"),
                 .process("Resources/phrases_should_add.txt"),
                 .process("Resources/phrases_should_remove.txt"),
