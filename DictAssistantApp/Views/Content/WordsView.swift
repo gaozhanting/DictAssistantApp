c029ef5845760c76905a160c106bb7a622bf3841
GaoZhanting <colorgmi@icloud.com>
2021-11-16 15:30:30 +0800

    f

--- DictAssistantApp/Views/Content/WordsView.swift
+++ DictAssistantApp/Views/Content/WordsView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct WordsView: View {
     @EnvironmentObject var displayedWords: DisplayedWords
-    @AppStorage(IsShowPhrasesKey) private var isShowPhrase: Bool = true // the value only used when the key doesn't exists, this will never be the case because we init it when app lanched
+    @AppStorage(IsShowPhrasesKey) private var isShowPhrase: Bool = true
     @AppStorage(IsShowCurrentKnownKey) private var isShowCurrentKnown: Bool = false
     @AppStorage(IsShowCurrentKnownButWithOpacity0Key) private var isShowCurrentKnownButWithOpacity0: Bool = false
     @AppStorage(IsShowCurrentNotFoundWordsKey) private var isShowCurrentNotFoundWords: Bool = false
