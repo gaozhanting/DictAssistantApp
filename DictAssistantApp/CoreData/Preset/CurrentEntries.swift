a72cc974c3aba390cfdf406ab0f0bf2a51a3dfb7
GaoZhanting <colorgmi@icloud.com>
2021-11-25 22:33:37 +0800

    doing: add many languges entries here

---
+++ DictAssistantApp/CoreData/Preset/CurrentEntries.swift
@@ -0,0 +1,11 @@
+//
+//  CurrentEntries.swift
+//  DictAssistantApp
+//
+//  Created by Gao Cong on 2021/11/25.
+//
+
+import Foundation
+
+// when switch language, re-init this
+var currentEntries: Dictionary<String, String> = getAllZhSEntries()
