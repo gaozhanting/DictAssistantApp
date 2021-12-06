bbac7f13349f7968b53b4be307691694a9c46933
GaoZhanting <colorgmi@icloud.com>
2021-12-06 22:46:57 +0800

    fix: revert old, I think old is better here.

--- DictAssistantApp/Views/Content/SingleWordView.swift
+++ DictAssistantApp/Views/Content/SingleWordView.swift
@@ -32,20 +32,24 @@ private struct TextBodyTwisted: View {
         case .portrait:
             TextBody(wordCell: wordCell)
         case .landscape:
-            switch LandscapeStyle(rawValue: landscapeStyle)! {
-            case .normal, .autoScrolling:
+            if !wordCell.trans.isEmpty {
+                switch LandscapeStyle(rawValue: landscapeStyle)! {
+                case .normal, .autoScrolling:
+                    TextBody(wordCell: wordCell)
+                        .frame(
+                            maxWidth: CGFloat(landscapeMaxWidth),
+                            maxHeight: .infinity,
+                            alignment: .topLeading
+                        )
+                case .centered:
+                    TextBody(wordCell: wordCell)
+                        .frame(
+                            maxWidth: CGFloat(landscapeMaxWidth),
+                            alignment: .top
+                        )
+                }
+            } else {
                 TextBody(wordCell: wordCell)
-                    .frame(
-                        maxWidth: CGFloat(landscapeMaxWidth),
-                        maxHeight: .infinity,
-                        alignment: .topLeading
-                    )
-            case .centered:
-                TextBody(wordCell: wordCell)
-                    .frame(
-                        maxWidth: CGFloat(landscapeMaxWidth),
-                        alignment: .top
-                    )
             }
         }
     }
