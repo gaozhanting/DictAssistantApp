6694649e677fa2eeaa80b17c91809f78b6e13404
GaoZhanting <colorgmi@icloud.com>
2021-12-28 15:29:20 +0800

    f: delete content info

--- DictAssistantApp/Views/Settings/ScenarioSettingsItemViews/ContentLayoutSettingsView.swift
+++ DictAssistantApp/Views/Settings/ScenarioSettingsItemViews/ContentLayoutSettingsView.swift
@@ -68,18 +68,3 @@ struct ContentLayoutStyleSettingsView: View {
         }
     }
 }
-
-private struct InfoView: View {
-    var body: some View {
-        Text("If the content is empty when you did run playing, what you can do?\n1. Check if there indeed has some English text in cropper area \n2. Try to turn down the Minimum Text Height \n3. Try to use Text Recognition Level accurate level \n4. Check if the words in the cropper screen are not all in your known words list \n5. Make sure the Use Entry Mode is not selected to only when you have no custom entries, which will ignore builtIn dictionary and Apple Dictionary \n6. Make sure you have already built a dictionary successfully, or make sure you did selected a valid dictionary in your Apple Dictionary Preferences if you select to use Apple Dictionary \n7. Replay (when sometimes you switch screens) \n\nHere are some tips for you:\n1. When you changed the Apple Dictionary Preferences, you need to restart this App(not restart the Apple Dictionary App) in order to take effect.\n2. In the content, you can right click (or command click) the word, and there are commands for quick add/remove to known. Remove From Known will only be available when you toggle Show Known on.\n3. Some hotkeys: Option Click the word will add or remove to known; Command Click the word will speak the word using system speech voice; Shift Click the word will open the Apple Dictionary App of the word.")
-            .font(.callout)
-            .padding()
-            .frame(width: 520)
-    }
-}
-
-struct ContentSettingsView_Previews: PreviewProvider {
-    static var previews: some View {
-        InfoView()
-    }
-}
